2821a527cb3aa34aa95162854912fd89e0857cc0
listratov <listratov.m@gmail.com>
2014-06-02 02:30:48 +0400

    first commit li3

--- public_html/index-test.php
+++ public_html/index-test.php
@@ -4,12 +4,16 @@
  * This file should be removed when the application is deployed for production.
  */
 
+
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/../yii-1.1.14/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/test.php';
 
+
+
 // remove the following line when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
+
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
